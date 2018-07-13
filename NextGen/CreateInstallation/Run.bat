a3510342a2135dd7fc1ad16b191b3dc491d0fb65
Dean Holzworth <Dean.Holzworth@csiro.au>
2018-07-13 12:41:16 +1000

    Add script to create installs

--- NextGen/CreateInstallation/Run.bat
+++ NextGen/CreateInstallation/Run.bat
@@ -15,5 +15,38 @@ cd \ApsimX
 git reset --hard %1
 
 rem Create installs
+echo.
+echo ########### Creating documentation
+cd \ApsimX\Documentation
+call GenerateDocumentation.bat
+IF ERRORLEVEL 1 GOTO END
+
+echo.
+echo ########### Creating Windows installation
 cd \ApsimX\Setup
-call BuildInstalls.bat
+"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" apsimx.iss
+IF ERRORLEVEL 1 GOTO END
+
+echo ########### Creating Debian package
+if exist \ApsimX\Setup\Linux\BuildDeb.bat (
+    cd \ApsimX\Setup\Linux
+    call BuildDeb.bat
+)
+IF ERRORLEVEL 1 GOTO END
+
+echo ########### Creating Mac OS X installation
+if exist "\ApsimX\Setup\OS X\BuildMacDist.bat" (
+cd "\ApsimX\Setup\OS X"
+call BuildMacDist.bat
+cd ..
+)
+IF ERRORLEVEL 1 GOTO END
+
+rem echo ########### Uploading installations
+rem cd "C:\Jenkins\workspace\1. GitHub pull request\ApsimX\Setup"
+rem call C:\Jenkins\ftpcommand.bat %Issue_Number%
+rem echo.
+rem 
+rem echo ########### Add a green build to DB
+rem set /p PASSWORD=<C:\Jenkins\ChangeDBPassword.txt
+rem curl -k https://www.apsim.info/APSIM.Builds.Service/Builds.svc/AddGreenBuild?pullRequestNumber=%ghprbPullId%^&buildTimeStamp=%DATETIMESTAMP%^&changeDBPassword=%PASSWORD%
