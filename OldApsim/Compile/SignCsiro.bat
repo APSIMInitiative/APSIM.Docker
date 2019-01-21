30b5ae480c3189a46d2d11970e800bf43945a802
NEXUS\hol430 <drew.holzworth@csiro.au>
2019-01-22 09:31:36 +1000

    Added a script to sign the compiled binaries.

---
+++ OldApsim/Compile/SignCsiro.bat
@@ -0,0 +1,8 @@
+@echo off
+set TIMESTAMP="http://timestamp.comodoca.com/?td=sha256"
+set SIGNTOOL="C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe"
+set SERTIFICATE="C:\new-code-signer.pfx"
+set p="c0d3rCSIR0"
+
+%SIGNTOOL% sign /q /as /fd sha256 /tr %TIMESTAMP% /td sha256 /f %SERTIFICATE% /p %p% %1
+%SIGNTOOL% verify /pa /v /d %1
