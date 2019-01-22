17bf0c55c6d66315afc82e8794bb50bc03bea071
NEXUS\hol430 <drew.holzworth@csiro.au>
2019-01-22 10:24:42 +1000

    Added signtool.exe to old apsim docker container.

--- OldApsim/Compile/SignCsiro.bat
+++ OldApsim/Compile/SignCsiro.bat
@@ -1,6 +1,6 @@
 @echo off
 set TIMESTAMP="http://timestamp.comodoca.com/?td=sha256"
-set SIGNTOOL="C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe"
+set SIGNTOOL="C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x64\signtool.exe"
 set SERTIFICATE="C:\new-code-signer.pfx"
 set p="c0d3rCSIR0"
 
