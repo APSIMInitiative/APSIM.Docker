90f55c9fbec79e6814b67a2f99422eaa3140b24c
NEXUS\hol430 <drew.holzworth@csiro.au>
2019-02-15 08:23:42 +1000

    Reduced curl verbosity when uploading files to Bob.

--- OldApsim/Compile/compile.bat
+++ OldApsim/Compile/compile.bat
@@ -40,9 +40,9 @@ rem Upload installers to Bob.
 cd %APSIM%\Release
 dir
 echo Uploading %sha1%.binaries.WINDOWS.INTEL.exe...
-@curl -u %BOB_CREDS% -T %sha1%.binaries.WINDOWS.INTEL.exe ftp://bob.apsim.info/Files/
+@curl -s -u %BOB_CREDS% -T %sha1%.binaries.WINDOWS.INTEL.exe ftp://bob.apsim.info/Files/
 echo Uploading %sha1%.binaries.WINDOWS.X86_64.exe
-@curl -u %BOB_CREDS% -T %sha1%.binaries.WINDOWS.X86_64.exe ftp://bob.apsim.info/Files/
+@curl -s -u %BOB_CREDS% -T %sha1%.binaries.WINDOWS.X86_64.exe ftp://bob.apsim.info/Files/
 
 rem Create diffs and binary archive.
 %APSIM%/Model/CreateDiffZip.exe Directory=%APSIM% PatchFileName=%sha1%
@@ -51,6 +51,6 @@ rem Create diffs and binary archive.
 
 rem Upload diffs and binary archive to Bob.
 echo Uploading C:\%sha1%.buildtree.zip...
-@curl -u %BOB_CREDS% -T C:\%sha1%.buildtree.zip ftp://bob.apsim.info/Files/
+@curl -s -u %BOB_CREDS% -T C:\%sha1%.buildtree.zip ftp://bob.apsim.info/Files/
 echo Uploading C:\%sha1%.binaries.zip...
-@curl -u %BOB_CREDS% -T C:\%sha1%.binaries.zip ftp://bob.apsim.info/Files/
+@curl -s -u %BOB_CREDS% -T C:\%sha1%.binaries.zip ftp://bob.apsim.info/Files/
