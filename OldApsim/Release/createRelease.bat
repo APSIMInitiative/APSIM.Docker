71b5e5b457d0275d54c10175942f7e39e6c6ce1f
hol430 <drew.holzworth@csiro.au>
2020-04-06 11:32:24 +1000

    Incrementing revision number prototype code.

--- OldApsim/Release/createRelease.bat
+++ OldApsim/Release/createRelease.bat
@@ -3,14 +3,14 @@ setlocal enableDelayedExpansion
 
 rem ----- Get revision number for this build.
 echo Adding build to builds DB...
-@curl -sk "http://apsimdev.apsim.info/APSIM.Builds.Service/BuildsClassic.svc/GetRevisionNumber?PullID=!PULL_ID!^&DbConnectPassword=!DB_CONN_PSW!" > temp.txt
+@curl -sk "http://apsimdev.apsim.info/APSIM.Builds.Service/BuildsClassic.svc/GetLatestRevisionNo" > temp.txt
 
 if errorlevel 1 (
 	echo Error: Unable to add build to Builds DB.
 	exit /b 1
 )
 
-rem ----- Read response (Job ID) from server.
+rem ----- Read response (Revision number) from server.
 for /F "tokens=1-6 delims==><" %%I IN (temp.txt) DO SET REVISION_NUMBER=%%K
 set RES=F
 if errorlevel 1 set RES=T 			rem if errorlevel 1
@@ -22,6 +22,9 @@ if "!RES!"=="T" (
 	exit 1
 )
 
+rem ----- Increment revision number.
+set /a REVISION_NUMBER=%REVISION_NUMBER%+1 >nul
+
 rem ----- Display Job ID and delete temp file.
 set REVISION_NUMBER
 del temp.txt
@@ -31,3 +34,9 @@ set "PatchFileNameShort=Apsim710-r%REVISION_NUMBER%"
 set "TARGET=JenkinsRelease"
 
 call Docker\\OldApsim\\jenkins.bat
+if errorlevel 1 exit /b 1
+
+rem ----- Update revision number for this pull request.
+echo Updating revision number for pull request #%PULL_ID% to %REVISION_NUMBER%
+@curl -sk "http://apsimdev.apsim.info/APSIM.Builds.Service/BuildsClassic.svc/UpdateRevisionNumberForPR?pullRequestID=%PULL_ID%^&revisionNumber=%REVISION_NUMBER%^&DbConnectPassword=!DB_CONN_PSW!" > temp.txt
+endlocal
