f6c358bca435315801e5be5b325dec5de276e2e9
Drew Holzworth <drew.holzworth@csiro.au>
2020-06-12 15:05:08 +1000

    Fixed docker files path.

--- OldApsim/jenkins.bat
+++ OldApsim/jenkins.bat
@@ -2,7 +2,7 @@
 setlocal enableDelayedExpansion
 
 rem ----- Copy signing files
-copy /y C:\signapsimx\* include\
+copy /y C:\signapsimx\* Docker\OldApsim\include\
 copy /y C:\\new-code-signer.pfx Docker\\OldApsim\\include\\>nul
 copy /y C:\\dbConnect.txt Docker\\OldApsim\\include\\>nul
 if errorlevel 1 (
